3ee1e336d684f9d4337905475313cc1d5c9b0cba
Carlo Wood <carlo@alinoe.com>
2019-12-02 23:16:24 +0100

    rewrite_header: add module name and desc.

--- DnsResolver.cxx
+++ DnsResolver.cxx
@@ -1,4 +1,6 @@
 /**
+ * resolver-task -- AIStatefulTask submodule - asynchronous hostname resolver.
+ *
  * @file
  * @brief Implementation of DnsResolver.
  *
@@ -7,18 +9,20 @@
  * RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
  * Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of resolver-task.
+ *
+ * Resolver-task is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Resolver-task is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with resolver-task.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "sys.h"
@@ -28,7 +32,6 @@
 #include "threadsafe/aithreadsafe.h"
 #include "utils/NodeMemoryPool.h"
 #include "utils/AIAlert.h"
-#include "evio/StreamBuf-threads.h"
 #include <arpa/inet.h>
 #include <cstring>
 
